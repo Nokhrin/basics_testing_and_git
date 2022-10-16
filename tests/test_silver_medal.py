13283608a359117d662f756ddca1a630a814833b
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-10-16 17:28:21 +0500

    finished test for silver_medal

--- tests/test_silver_medal.py
+++ tests/test_silver_medal.py
@@ -5,8 +5,8 @@ from informatics.linear_search.silver_medal import get_second_max
 @pytest.mark.parametrize(
     argnames='actual_input, expected_output',
     argvalues=[
-        (('5', '4 3 3 1 2'), '3'),
-        (('8', '1 2 5 3 5 6 6 5'), '5')
+        ((5, [4, 3, 3, 1, 2]), 3),
+        ((8, [1, 2, 5, 3, 5, 6, 6, 5]), 5)
     ]
 )
 def test_get_second_max(actual_input: tuple, expected_output: str) -> None:
