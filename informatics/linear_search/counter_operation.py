810de007ec6b81c93aa9db7be0cfb65660950edc
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2022-10-16 16:19:26 +0500

    added problem

---
+++ informatics/linear_search/counter_operation.py
@@ -0,0 +1,46 @@
+"""
+Контроперация
+Ввод и вывод данных производятся через стандартные потоки ввода-вывода.
+
+Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные.
+Напишите программу, которая заменяет оценки Василия, но наоборот (все максимальные - на минимальные).
+
+Входные данные
+Дано количество оценок Василия (не больше 100), затем сами оценки.
+
+Выходные данные
+Требуется вывести исправленные оценки в том же порядке.
+"""
+import sys
+
+
+def replace_max_with_min(marks: str) -> str:
+    """Return numbers with max replaced by min value"""
+    tmp_list = [int(mark) for mark in marks.split(sep=' ')]
+    marks_count = tmp_list[0]
+    marks_list = tmp_list[1:]
+
+    max_value = -1
+    min_value = 6
+
+    for i in range(marks_count):
+        if marks_list[i] > max_value:
+            max_value = marks_list[i]
+        if marks_list[i] < min_value:
+            min_value = marks_list[i]
+
+    max_values_indexes = []
+    for i in range(marks_count):
+        if marks_list[i] == max_value:
+            max_values_indexes.append(i)
+
+    for max_index in max_values_indexes:
+        marks_list[max_index] = min_value
+
+    return ' '.join([str(num) for num in marks_list])
+
+
+if __name__ == '__main__':
+    print(replace_max_with_min(input()))
+    # print(replace_max_with_min('5 1 3 3 3 4'))  # 1 3 3 3 1
+    # print(replace_max_with_min('8 5 4 2 2 4 2 2 5'))  # 2 4 2 2 4 2 2 2
